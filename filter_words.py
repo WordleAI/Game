78bb46d15b4041999dcda121dfc77267d9d52c5f
Gureet Sidhu <42849884+gs109111@users.noreply.github.com>
2026-03-01 20:16:28 -0700

    Added a comment

--- filter_words.py
+++ filter_words.py
@@ -5,5 +5,5 @@ def filter_from_wordlist(word_list):
     # Filter words that are not 5 words
     with open(word_list, "r") as f, open("words_filtered.txt", "w") as f2:
         for line in f:
-            if len(line.strip()) == 5:
+            if len(line.strip()) == 5: # Remove words with less than or greater than 5 letters
                 f2.write(line)
