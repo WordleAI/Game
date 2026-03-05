3e7cdf2026ff82560e54f6f1d89ef2202f2b4d59
Gureet Sidhu <42849884+gs109111@users.noreply.github.com>
2026-03-05 16:50:47 -0700

    Update filter_words.py

--- filter_words.py
+++ filter_words.py
@@ -1,5 +1,5 @@
 # Filter Words
-# A modified version of https://github.com/gs109111/WordleSolver/blob/dictionary/filter_words.py 
+# A modified version of https://github.com/WordleAI/Dictionary/raw/refs/heads/dictionary/filter_words.py
 
 def filter_from_wordlist(word_list):
     # Filter words that are not 5 words
